3d224552cbaa7a518b24ae7f0e0c3d7d059cd235
Chris.Sloan <Chris.Sloan@global.com>
2022-09-30 19:50:01 +0100

    Fixes compile, minimum and target sdk versions.

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,7 +1,7 @@
 object ConfigData {
-    const val compileSdk = 30
-    const val minSdk = 21
-    const val targetSdk = 30
+    const val compileSdk = 33
+    const val minSdk = 23
+    const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
 }
