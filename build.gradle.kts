7556ab9c049f0f6cd1b296a6df7d037fd5db5d32
Chris Sloan <chris.sloan@totatterrain.eu>
2022-10-01 09:28:24 +0100

    Updates to java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id ("com.android.application") version "7.3.0" apply false
     id ("com.android.library") version "7.3.0" apply false
@@ -5,6 +7,14 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.10" apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
 }
